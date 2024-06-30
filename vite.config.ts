7d384ccbf28f673b2e27cb392627abef5d5f12d8
luisguilherme <lsguilhermest@gmail.com>
2024-06-30 19:36:28 -0300

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ include: ['lib'] })],
+  plugins: [react(), svgr(), dts({ include: ['lib'] })],
   build: {
     copyPublicDir: false,
     lib: {
