b10bb92f11e1598075a9668f990c2378133a4602
luisguilherme <lsguilhermest@gmail.com>
2024-07-01 11:42:50 -0300

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
+import { extname, relative, resolve } from 'path'
+import { fileURLToPath } from 'node:url'
 import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
-import svgr from "vite-plugin-svgr";
+import svgr from 'vite-plugin-svgr'
+import { glob } from 'glob'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,11 +12,35 @@ export default defineConfig({
   build: {
     copyPublicDir: false,
     lib: {
-      entry: resolve(__dirname, 'lib/main.ts'),
-      formats: ['es']
+      entry: resolve(__dirname, 'lib/index.ts'),
+      formats: ['es'],
+      fileName: 'index'
     },
     rollupOptions: {
-      external: ['react', 'react/jsx-runtime']
+      external: ['react', 'react/jsx-runtime'],
+      input: Object.fromEntries(
+        // https://rollupjs.org/configuration-options/#input
+        glob.sync('lib/**/*.{ts,tsx}', {
+          ignore: ["lib/**/*.d.ts"],
+        }).map(file => [
+          // 1. The name of the entry point
+          // lib/nested/foo.js becomes nested/foo
+          relative(
+            'lib',
+            file.slice(0, file.length - extname(file).length)
+          ),
+          // 2. The absolute path to the entry file
+          // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
+          fileURLToPath(new URL(file, import.meta.url))
+        ])
+      ),
+      output: {
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: '[name].js',
+      }
     }
+  },
+  server: {
+    port: 3000
   }
 })
