b12551ad4fd51a16af7f7e75cfb396bc71c70f0e
luisguilherme <lsguilhermest@gmail.com>
2024-07-10 13:56:11 -0300

    add new icons, update icon name

--- lib/svg/circle/index.ts
+++ lib/svg/circle/index.ts
@@ -1,51 +1,53 @@
-import CircleBolt from './circle-bolt.svg?react';
-import CircleCheck from './circle-check.svg?react';
+import BoltCircle from './bolt-circle.svg?react';
+import CheckCircle from './check-circle.svg?react';
 import CircleDashed from './circle-dashed.svg?react';
-import CircleDollar from './circle-dollar.svg?react';
+import DollarCircle from './dollar-circle.svg?react';
 import CircleDot from './circle-dot.svg?react';
-import CircleLocation from './circle-location.svg?react';
-import CircleMinus from './circle-minus.svg?react';
-import CirclePause from './circle-pause.svg?react';
-import CirclePercentage from './circle-percentage.svg?react';
-import CirclePlay from './circle-play.svg?react';
-import CirclePlus from './circle-plus.svg?react';
+import LocationCircle from './location-circle.svg?react';
+import MinusCircle from './minus-circle.svg?react';
+import PauseCircle from './pause-circle.svg?react';
+import PercentageCircle from './percentage-circle.svg?react';
+import PlayCircle from './play-circle.svg?react';
+import PlusCircle from './plus-circle.svg?react';
 import CircleSquare from './circle-square.svg?react';
 import CircleStop from './circle-stop.svg?react';
 import CircleUser from './circle-user.svg?react';
 import CircleXmark from './circle-xmark.svg?react';
 import Circle from './circle.svg?react';
+import InfoCircle from "./info-circle.svg?react";
 
 export default {
-  'circle-bolt': CircleBolt,
-  'circle-check': CircleCheck,
+  'bolt-circle': BoltCircle,
+  'check-circle': CheckCircle,
   'circle-dashed': CircleDashed,
-  'circle-dollar': CircleDollar,
+  'dollar-circle': DollarCircle,
   'circle-dot': CircleDot,
-  'circle-location': CircleLocation,
-  'circle-minus': CircleMinus,
-  'circle-pause': CirclePause,
-  'circle-percentage': CirclePercentage,
-  'circle-play': CirclePlay,
-  'circle-plus': CirclePlus,
+  'location-circle': LocationCircle,
+  'minus-circle': MinusCircle,
+  'pause-circle': PauseCircle,
+  'percentage-circle': PercentageCircle,
+  'play-circle': PlayCircle,
+  'plus-circle': PlusCircle,
   'circle-square': CircleSquare,
   'circle-stop': CircleStop,
   'circle-user': CircleUser,
   'circle-xmark': CircleXmark,
   'circle': Circle,
+  'info-circle': InfoCircle
 }
 
 export {
-  CircleBolt,
-  CircleCheck,
+  BoltCircle,
+  CheckCircle,
   CircleDashed,
-  CircleDollar,
+  DollarCircle,
   CircleDot,
-  CircleLocation,
-  CircleMinus,
-  CirclePause,
-  CirclePercentage,
-  CirclePlay,
-  CirclePlus,
+  LocationCircle,
+  MinusCircle,
+  PauseCircle,
+  PercentageCircle,
+  PlayCircle,
+  PlusCircle,
   CircleSquare,
   CircleStop,
   CircleUser,
--- lib/svg/general/index.ts
+++ lib/svg/general/index.ts
@@ -1,42 +1,66 @@
+import AlignJustify from './align-justify.svg?react';
+import AlignLeft from './align-left.svg?react';
+import AlignRight from './align-right.svg?react';
 import CheckDouble from "./check-double.svg?react";
 import Check from "./check.svg?react";
 import Eye from "./eye.svg?react";
 import EyeOff from "./eye-off.svg?react";
+import Grid from "./grid.svg?react";
 import Home from "./home.svg?react";
 import Lock from "./lock.svg?react";
 import Mail from "./mail.svg?react";
 import MailOpen from "./mail-open.svg?react";
+import Maximize from "./maximize.svg?react";
+import Minimize from "./minimize.svg?react";
 import Phone from "./phone.svg?react";
 import Settings from "./settings.svg?react";
 import Trash from "./trash.svg?react";
+import ThreeDotsHorizontal from "./three-dots-horizontal.svg?react";
+import ThreeDotsVertical from "./three-dots-vertical.svg?react";
 import Unlock from "./unlock.svg?react";
 
 export default {
+  'align-justify': AlignJustify,
+  'align-left': AlignLeft,
+  'align-right': AlignRight,
   'check-double': CheckDouble,
   'check': Check,
   'eye': Eye,
   'eye-off': EyeOff,
   'home': Home,
+  'grid': Grid,
   'lock': Lock,
   'mail': Mail,
   'mail-open': MailOpen,
+  'maximize': Maximize,
+  'minimize': Minimize,
   'phone': Phone,
   'settings': Settings,
   'trash': Trash,
+  'three-dots-horizontal': ThreeDotsHorizontal,
+  'three-dots-vertical': ThreeDotsVertical,
   'unlock': Unlock,
 }
 
 export {
+  AlignJustify,
+  AlignLeft,
+  AlignRight,
   CheckDouble,
   Check,
   Eye,
   EyeOff,
   Home,
+  Grid,
   Lock,
   Mail,
   MailOpen,
+  Maximize,
+  Minimize,
   Phone,
   Settings,
   Trash,
+  ThreeDotsHorizontal,
+  ThreeDotsVertical,
   Unlock
 }
