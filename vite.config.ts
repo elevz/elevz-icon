d0407d9898337de113648f755789d8158e2aaf21
luisguilherme <lsguilhermest@gmail.com>
2024-08-02 15:52:39 -0300

    fix: change build

--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,33 @@
 import { defineConfig } from 'vite'
-import { extname, relative, resolve } from 'path'
-import { fileURLToPath } from 'node:url'
 import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
 import svgr from 'vite-plugin-svgr'
-import { globSync } from 'glob'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     copyPublicDir: false,
-    sourcemap: false,
+    sourcemap: true,
+    emptyOutDir: true,
     lib: {
-      entry: resolve(__dirname, 'lib/index.ts'),
-      name: 'MyLib',
-      formats: ['es'],
-      fileName: 'my-lib'
+      entry: path.resolve(__dirname, "lib/index.ts"),
+      name: "elevz-icon",
+      fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
-      external: ['react', 'react/jsx-runtime'],
-      input: Object.fromEntries(
-        // https://rollupjs.org/configuration-options/#input
-        globSync('lib/**/*.{ts,tsx}', { ignore: ["lib/**/*.d.ts"] }).map(file => [
-          relative('lib', file.slice(0, file.length - extname(file).length)),
-          fileURLToPath(new URL(file, import.meta.url))
-        ])
-      ),
+      external: ["react", "react-dom"],
       output: {
-        entryFileNames: '[name].js',
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM"
+        },
       }
     }
   },
   plugins: [
     react(),
     svgr(),
-    dts({ include: ['lib'] })
+    dts({ tsconfigPath: './tsconfig.build.json' })
   ]
 })
